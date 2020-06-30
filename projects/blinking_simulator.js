00f9c68873cf9635ebd1ba0403a4406544566d32
ethan-k64 <59579977+ethan-k64@users.noreply.github.com>
2020-06-29 23:07:11 -0400

    Update blinking_simulator.js

--- projects/blinking_simulator.js
+++ projects/blinking_simulator.js
@@ -8,7 +8,7 @@ function setup() {
   cnv.position(displayWidth / 2 - 200, 100);
   
   stick = select("#stick");
-  stick.position(displayWidth / 2 - 100, 300);
+  stick.position(displayWidth / 2 - 100, 200);
   
   col = color(255);
 }
